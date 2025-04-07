21a9daf4d612be95a98edf0294ef9c9ee68be64f
SHI JINGYAO <31977556+qq490800573@users.noreply.github.com>
2025-04-07 17:07:42 +0900

    Update client_manager_main.py

--- client_manager_main.py
+++ client_manager_main.py
@@ -53,9 +53,9 @@ class manager_status(BaseModel):
 
     # FL_client: str = '0.0.0.0:8003'
     if len(sys.argv) == 1:
-        FL_client = 'localhost:8003'
+        FL_client: str = 'localhost:8003'
     else:
-        FL_client = 'fl-client:8003'
+        FL_client: str = 'fl-client:8003'
     server_ST: str = 'ccl.gachon.ac.kr:40019'
     server: str = 'ccl.gachon.ac.kr'
     S3_bucket: str = 'fl-gl-model'
